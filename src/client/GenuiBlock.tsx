38dc140d49032c73f6a2d2b69b22adaac7f081dc
Kunping Wang <146500293+wang-kaopu@users.noreply.github.com>
2026-09-06 13:58:15 +0800

    style(input): 应用颜色输入专用样式

--- src/client/GenuiBlock.tsx
+++ src/client/GenuiBlock.tsx
@@ -9,6 +9,7 @@
 import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useGenuiAction } from './action-context.ts'
 import css from './GenuiBlock.module.css'
+import './GenuiColorInput.css'
 import { loadBlockState, saveBlockState } from './interaction-store.ts'
 import { recordFence, recordInteraction } from './achievement-store.ts'
 import { renderNode } from './blocks/render-node.tsx'
